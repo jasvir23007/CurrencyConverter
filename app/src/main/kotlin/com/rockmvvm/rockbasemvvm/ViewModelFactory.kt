0476764c836fe7ff836f98fede371276dd465abe
Jasvir <jasvirpratap.singh@jungleworks.com>
2019-09-10 18:35:03 +0530

    redundant code removal

--- app/src/main/kotlin/com/rockmvvm/rockbasemvvm/ViewModelFactory.kt
+++ app/src/main/kotlin/com/rockmvvm/rockbasemvvm/ViewModelFactory.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.rockmvvm.rockbasemvvm.data.DataManager
 import com.rockmvvm.rockbasemvvm.ui.currencyconverter.CurrencyViewModel
-import com.rockmvvm.rockbasemvvm.ui.post.PostListViewModel
 import com.rockmvvm.rockbasemvvm.util.rx.SchedulerProvider
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -24,10 +23,6 @@ class ViewModelFactory
     ViewModelProvider.NewInstanceFactory() {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(PostListViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return PostListViewModel(mDataManager, mSchedulerProvider) as T
-        }
 
         if (modelClass.isAssignableFrom(CurrencyViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
--- app/src/main/kotlin/com/rockmvvm/rockbasemvvm/di/builder/ActivityBuilder.kt
+++ app/src/main/kotlin/com/rockmvvm/rockbasemvvm/di/builder/ActivityBuilder.kt
@@ -1,7 +1,6 @@
 package com.rockmvvm.rockbasemvvm.di.builder
 
 import com.rockmvvm.rockbasemvvm.ui.currencyconverter.ActivityConverter
-import com.rockmvvm.rockbasemvvm.ui.post.PostListActivity
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -13,9 +12,6 @@ import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class ActivityBuilder {
-
-    @ContributesAndroidInjector
-    abstract fun bindPostListActivity(): PostListActivity
 
     @ContributesAndroidInjector
     abstract fun bindConverterActivity(): ActivityConverter
